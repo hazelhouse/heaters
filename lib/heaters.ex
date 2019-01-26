c320ed8ea7f096fc2dea1425e8530305772ca969
Sidney Kochman <sidney@kochman.org>
2019-01-26 16:25:02 -0500

    create nerves project

---
+++ lib/heaters.ex
@@ -0,0 +1,18 @@
+defmodule Heaters do
+  @moduledoc """
+  Documentation for Heaters.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Heaters.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
